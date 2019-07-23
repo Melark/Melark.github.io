cfa83ce2d977d0ec4e97ada3f133fcd5247ac83e
Mark Arrow <markarrow6@gmail.com>
2019-07-23 09:10:31 +0200

    Main page initial WIP

--- SRC/src/app/app-routing.module.ts
+++ SRC/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'social', component: SocialMediaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SRC/src/app/app.module.ts
+++ SRC/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
+import { ProfilePictureComponent } from './layout/profile-picture/profile-picture.component';
+import { ProfileCardComponent } from './layout/profile-card/profile-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    SocialMediaComponent,
+    ProfilePictureComponent,
+    ProfileCardComponent,
   ],
   imports: [
     BrowserModule,
